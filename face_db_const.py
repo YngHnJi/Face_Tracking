7302e171bacc3f43f67bee8074b1b4a90a95cc45
YngHnJi <39265784+YngHnJi@users.noreply.github.com>
2019-09-28 13:59:19 +0900

    Add files via upload

--- face_db_const.py
+++ face_db_const.py
@@ -1,24 +1,6 @@
 import cv2
-import numpy as np
 import os
-
-face_classifier = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-
-
-def face_extractor(img):
-
-    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-    faces = face_classifier.detectMultiScale(gray,1.3,5)
-
-    if faces is():
-        return img, None
-
-    for(x,y,w,h) in faces:
-        cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 255), 2)
-        cropped_face = img[y:y+h, x:x+w]
-
-
-    return img, cropped_face
+import face_detect
 
 
 def face_db_const(username):
@@ -37,15 +19,12 @@ def face_db_const(username):
     while True:
         ret, frame = video.read()
         cv2.imshow("frame", frame)
-        frame, detected_face = face_extractor(frame)
+        frame, detected_face, _ = face_detect.face_detector_v2(frame)
         if detected_face is not None:
             count += 1
-            face = cv2.resize(detected_face, (200, 200))
-            face = cv2.cvtColor(face, cv2.COLOR_BGR2GRAY)
-
             file_name_path = "face_db_const/" + username + "/" + str(count) + '.jpg'
 
-            cv2.imwrite(file_name_path, face)
+            cv2.imwrite(file_name_path, detected_face)
 
             cv2.putText(frame, str(count), (50, 50), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 2)
             print("Num of Photo collected : ", count)
